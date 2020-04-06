2020421c8715b061bd7dcce5c7bdc5c9a7c60e98
Dylan Smith <Dylansmithcg@gmail.com>
2020-04-06 12:27:32 -0700

    getfoobybar done

--- php/lib/author.php
+++ php/lib/author.php
@@ -14,6 +14,15 @@ public function insert(\PDO $pdo) {
 	$statement->execute($parameters);
 }
 
+public function update(\PDO $pdo) {
+	$query = "UPDATE Author SET authorActivationToken = :authorActivationToken, authorAvatarUrl = :authorAvatarUrl, authorEmail = :authorEmail, authorEmail = :authorEmail, authorHash = :authorHash, authorUsername = :authorUsername WHERE authorId = :authorId";
+	$statement = $pdo->prepare($query);
+
+	$parameters = ["authorId" => $this->authorId->getBytes(), "authorActivationToken" => $this->authorActivationToken, "authorAvatarUrl" => $this->authorAvatarUrl, "authorEmail" => $this->authorEmail, "authorHash" => $this->authorHash, "authorUsername" => $this->authorUsername];
+	$statement->execute($parameters);
+}
+
+
 public function delete(\PDO $pdo) {
 	$query = "DELETE FROM Author WHERE authorId = :authorId";
 	$statement = $pdo->prepare($query);
@@ -22,11 +31,30 @@ public function delete(\PDO $pdo) {
 	$statement->execute($parameters);
 }
 
-public function update(\PDO $pdo) {
-	$query = "UPDATE Author SET authorActivationToken = :authorActivationToken, authorAvatarUrl = :authorAvatarUrl, authorEmail = :authorEmail, profileEmail = :profileEmail, authorHash = :authorHash, authorUsername = :authorUsername WHERE authorId = :authorId";
+public static function getAuthorByAuthorId(\PDO $pdo, $authorId):Author {
+	try {
+		$authorId = self::validateUuid($authorId);
+	} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+		throw(new \PDOException($exception->getMessage(), 0, $exception));
+	}
+	
+	$query = "SELECT authorId, authorActivationToken, authorAvatarUrl, authorEmail, authorHash, authorUsername FROM author WHERE authorId = :authorId";
 	$statement = $pdo->prepare($query);
-
-	$parameters = ["authorId" => $this->authorId->getBytes(), "authorActivationToken" => $this->authorActivationToken, "authorAvatarUrl" => $this->authorAvatarUrl, "authorEmail" => $this->authorEmail, "authorHash" => $this->authorHash, "authorUsername" => $this->authorUsername];
+	
+	$parameters = ["authorId" => $authorId->getBytes()];
 	$statement->execute($parameters);
+	
+	try {
+		$author = null;
+		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+		$row = $statement->fetch();
+		if($row !== false) {
+
+			$author = new Author($row["authorId"], $row["authorActivationToken"], $row["authorAvatarUrl"],$row["authorEmail"], $row["authorHash"], $row["authorUsername"]);
+		}
+	} catch(\Exception $exception) {
+		throw(new \PDOException($exception->getMessage(), 0, $exception));
+	}
+	return ($author);
 }
 
