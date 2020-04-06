b500221c310f6f353fd55aa05062282d3702153d
Dylan Smith <Dylansmithcg@gmail.com>
2020-04-06 12:16:52 -0700

    got 3 statements done

--- php/lib/author.php
+++ php/lib/author.php
@@ -4,23 +4,29 @@ require_once(dirname(__DIR__) . "/Classes/autoload.php");
 
 
 
-use DylanSmithcg\ObjectOriented\{author};
+use DylanSmithcg\ObjectOriented\php\Classes\{Auhtor};
 
+public function insert(\PDO $pdo) {
+	$query = "INSERT INTO Author(authorId, authorActivationToken, authorAvatarUrl, authorEmail, authorHash, authorUsername) VALUES (:authorId, :authorAcivationToken, :authorAvatarUrl, :authorEmail, :authorHash, :authorUsername)";
+	$statement = $pdo->prepare($query);
 
-
-	public function bar() {
-
-	$authorId = "uuid4";
-	$authorActivationToken = "numbers";
-	$authorAvatarUrl = "link.png";
-	$authorEmail = "email@gmail.com";
-	$authorHash = "stuff";
-	$authorUsername = "something";
-	echo "$authorEmail $authorActivationToken authorHash";
-
-	$author = new author(authorId, $authorActivationToken, $authorAvatarUrl, authorEmail, author, $authorUsername);
-	echo var_dump($author);
-	echo getAuthorUsername();
-
-	echo 'i work';
+	$parameters = ["authorId" => $this->authorId->getBytes(), "authorActivationToken" => $this->authorActivationToken, "authorAvatarUrl" => $this->authorAvatarUrl, "authorEmail" => $this->authorEmail, "authorHash" => $this->authorHash, "authorUsername" => $this->authorUsername];
+	$statement->execute($parameters);
 }
+
+public function delete(\PDO $pdo) {
+	$query = "DELETE FROM Author WHERE authorId = :authorId";
+	$statement = $pdo->prepare($query);
+
+	$parameters = ["authorId" => $this->auhtorId->getBytes()];
+	$statement->execute($parameters);
+}
+
+public function update(\PDO $pdo) {
+	$query = "UPDATE Author SET authorActivationToken = :authorActivationToken, authorAvatarUrl = :authorAvatarUrl, authorEmail = :authorEmail, profileEmail = :profileEmail, authorHash = :authorHash, authorUsername = :authorUsername WHERE authorId = :authorId";
+	$statement = $pdo->prepare($query);
+
+	$parameters = ["authorId" => $this->authorId->getBytes(), "authorActivationToken" => $this->authorActivationToken, "authorAvatarUrl" => $this->authorAvatarUrl, "authorEmail" => $this->authorEmail, "authorHash" => $this->authorHash, "authorUsername" => $this->authorUsername];
+	$statement->execute($parameters);
+}
+
