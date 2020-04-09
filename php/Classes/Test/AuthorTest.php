482dbd990e1b898f978c48b11bd70fbd840ee7df
Dylan Smith <Dylansmithcg@gmail.com>
2020-04-09 14:39:22 -0700

    added hack

---
+++ php/Classes/Test/AuthorTest.php
@@ -0,0 +1,19 @@
+<?php
+namespace DylanSmithcg\ObjectOriented\Test;
+
+use DylanSmithcg\ObjectOriented\{Author};
+
+//hack
+require_once(dirname(__DIR__) . "/Test/DataDesignTest.php");
+
+// grab the class under scrutiny
+require_once(dirname(__DIR__) . "/autoload.php");
+
+// grab the uuid generator
+require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
+
+class AuthorTest extends DataDesignTest {
+
+
+
+}
